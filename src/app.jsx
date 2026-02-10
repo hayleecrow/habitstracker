77ccb9d97e728e3b24be3bd3e09db8f935410446
hayleecrow <cro21045@byui.edu>
2026-02-09 17:54:44 -0700

    Change class to className so it wouldn't conflict with the js class keyword

--- src/app.jsx
+++ src/app.jsx
@@ -5,19 +5,19 @@ import './app.css';
 export default function App() {
     return (
         <div className="body">
-            <header class="container-fluid">
-                <img class="logo" alt="Habit Go logo" src="habit_tracker_logo_transparent.png"/>
-                <nav class="navbar navbar-dark">
-                    <a class="navbar-brand text-reset" href="#">Habit Go<sup>&reg;</sup></a>
-                    <menu class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="index.html">Login</a>
+            <header className="container-fluid">
+                <img className="logo" alt="Habit Go logo" src="habit_tracker_logo_transparent.png"/>
+                <nav className="navbar navbar-dark">
+                    <a className="navbar-brand text-reset" href="#">Habit Go<sup>&reg;</sup></a>
+                    <menu className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" href="index.html">Login</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="my_habits.html">My Habits</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="my_habits.html">My Habits</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="friends.html">Friends</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="friends.html">Friends</a>
                         </li>
                     </menu>
                 </nav>
@@ -26,9 +26,9 @@ export default function App() {
             <main>App components go here</main>
 
             <footer>
-                <div class="container-fluid">
-                    <span class="text-reset">Author: Haylee Crow</span>
-                    <a class="text-reset" href="https://github.com/hayleecrow/habitstracker">GitHub</a>
+                <div className="container-fluid">
+                    <span className="text-reset">Author: Haylee Crow</span>
+                    <a className="text-reset" href="https://github.com/hayleecrow/habitstracker">GitHub</a>
                 </div>
             </footer>
         </div>
