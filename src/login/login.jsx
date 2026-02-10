56644a1bc568b0e5e2e90ab59fe891b8670e128c
hayleecrow <cro21045@byui.edu>
2026-02-09 18:47:23 -0700

    Copied over original html over into login component

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main className="container-fluid">
+        <h1>Welcome to Habit Go!</h1>
+        <form method="get" action="my_habits.html">
+            <div className="input-group mb-3">
+                <input className="form-control" type="text" placeholder="Email"/>
+            </div>
+            <div className="input-group mb-3">
+                <input className="form-control" type="password" placeholder="Password"/>
+            </div>
+            <button className="btn btn-primary" type="submit">Login</button>
+            <button className="btn" type="submit">Create Account</button>
+        </form>
     </main>
   );
 }
