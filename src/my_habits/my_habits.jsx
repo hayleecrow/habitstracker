3577d6d7927eafd799ab59313f06781bc8821d33
hayleecrow <cro21045@byui.edu>
2026-02-09 18:04:02 -0700

    Copied initial code from instructions

---
+++ src/my_habits/my_habits.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function MyHabits() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>my habits displayed here</div>
+    </main>
+  );
+}
