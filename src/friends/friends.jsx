92c2b637b17d0f2180de766519ea31ea678ebbde
hayleecrow <cro21045@byui.edu>
2026-02-09 20:26:09 -0700

    Copied over original html over into friends component and imported css

--- src/friends/friends.jsx
+++ src/friends/friends.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import './friends.css';
 
 export function Friends() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>friends displayed here</div>
+    <main className="container-fluid">
+        <h1>Friends</h1>
+        <table className="table table-warning">
+            <thead className="table-dark">
+                <tr>
+                    <th>Name</th><th>Streak</th><th>Habits</th>
+                </tr>
+            </thead>
+            <body>
+                <tr>
+                    <td>Sarah</td><td>20<span>🔥</span></td><td><details><summary>Details</summary><ul><li>10<span>🔥</span> Exercise - 10 min</li></ul></details></td>
+                </tr>
+                <tr>
+                    <td>Eric</td><td>50<span>🔥</span></td><td><details><summary>Details</summary></details></td>
+                </tr>
+                <tr>
+                    <td>Jack</td><td>4<span></span></td><td><details><summary>Details</summary></details></td>
+                </tr>
+            </body>
+        </table>
+        <button className="btn btn-primary" type="button">Add Friend +</button>
     </main>
   );
 }
