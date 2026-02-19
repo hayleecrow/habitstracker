b3d57a49ba407dae61ea0232b6d0e2f59623d1e0
hayleecrow <cro21045@byui.edu>
2026-02-18 17:12:52 -0700

    Created use state and on change elements for email and password

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
 export function Home() {
-  return (
+    const [email, setEmail] = React.useState(localStorage.getItem('email') || '');
+    const [password, setPassword] = React.useState(localStorage.getItem('password') || '');
+
+    return (
     <main className="container-fluid">
         <h1>Welcome to Habit Go!</h1>
         <form method="get" action="my_habits.html">
             <div className="input-group mb-3">
-                <input className="form-control" type="text" placeholder="Email"/>
+                <input className="form-control" type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
             </div>
             <div className="input-group mb-3">
-                <input className="form-control" type="password" placeholder="Password"/>
+                <input className="form-control" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
             </div>
             <button className="btn btn-primary" type="submit">Login</button>
             <button className="btn" type="submit">Create Account</button>
         </form>
     </main>
-  );
+    );
 }
