ec8962d5bbfcb1db52e751cf4aa0a5614131276d
hayleecrow <cro21045@byui.edu>
2026-02-18 17:39:12 -0700

    Reverted changing function name so it's back to login so that router works properly

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function Home() {
+export function Login() {
     const [email, setEmail] = React.useState(localStorage.getItem('email') || '');
     const [password, setPassword] = React.useState(localStorage.getItem('password') || '');
 
