b3eb47ddff6ea0b3b4434e93a7b311cbeb68a6e8
hayleecrow <cro21045@byui.edu>
2026-02-18 17:44:18 -0700

    Added login user function that runs when login button is clicked

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -4,6 +4,12 @@ export function Login() {
     const [email, setEmail] = React.useState(localStorage.getItem('email') || '');
     const [password, setPassword] = React.useState(localStorage.getItem('password') || '');
 
+    function loginUser() { 
+        console.log(`Logging in user with email: ${email} and password: ${password}`);
+        localStorage.setItem('user', email);
+        localStorage.setItem('password', password);
+    }
+
     return (
     <main className="container-fluid">
         <h1>Welcome to Habit Go!</h1>
@@ -14,7 +20,7 @@ export function Login() {
             <div className="input-group mb-3">
                 <input className="form-control" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
             </div>
-            <button className="btn btn-primary" type="submit">Login</button>
+            <button className="btn btn-primary" type="submit" onClick={loginUser}>Login</button>
             <button className="btn" type="submit">Create Account</button>
         </form>
     </main>
