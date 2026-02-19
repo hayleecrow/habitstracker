abf736403c78b251328b4542b15a15a8917ea60b
hayleecrow <cro21045@byui.edu>
2026-02-18 22:33:59 -0700

    Changes login view when email is or is not added

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -21,19 +21,35 @@ export function Login({ setUser }) {
         navigate('/my_habits');
     }
 
+    function logoutUser() { 
+        localStorage.removeItem('user');
+        localStorage.removeItem('password');
+        setUser(null);
+        location.reload();
+    }
+
     return (
     <main className="container-fluid">
         <h1>Welcome to Habit Go!</h1>
-        <form method="get">
-            <div className="input-group mb-3">
-                <input className="form-control" type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+        {!email && (
+            <form method="get">
+                <div className="input-group mb-3">
+                    <input className="form-control" type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div className="input-group mb-3">
+                    <input className="form-control" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <button className="btn btn-primary" type="submit" onClick={loginUser}>Login</button>
+                <button className="btn" type="submit" onClick={createUser}>Create Account</button>
+            </form>)
+        }
+        {email && (
+            <div>
+                <h2>{email}</h2>
+                <button className="btn btn-primary" onClick={() => navigate('/my_habits')}>My Habits</button>
+                <button className="btn" onClick={logoutUser}>Logout</button>
             </div>
-            <div className="input-group mb-3">
-                <input className="form-control" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-            </div>
-            <button className="btn btn-primary" type="submit" onClick={loginUser}>Login</button>
-            <button className="btn" type="submit" onClick={createUser}>Create Account</button>
-        </form>
+        )}
     </main>
     );
 }
