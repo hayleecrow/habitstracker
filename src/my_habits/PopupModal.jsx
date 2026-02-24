c452e0e3e55310b06ecff0510aa7c9e0fbe756e3
hayleecrow <cro21045@byui.edu>
2026-02-24 12:33:12 -0700

    Removed unneeded file that was going to be used for the popup

--- src/my_habits/PopupModal.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react"
-
-export function PopupModal() { 
-    return (
-        <div className="modal">
-            <h1>You're in the popup!</h1>
-        </div>
-    )
-}
