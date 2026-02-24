ac7cc203ac3533e88be70144a46ca53de0d18e52
hayleecrow <cro21045@byui.edu>
2026-02-23 20:08:20 -0700

    Initial commit with some initial text

---
+++ src/my_habits/PopupModal.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react"
+
+export function PopupModal() { 
+    return (
+        <div className="modal">
+            <h1>You're in the popup!</h1>
+        </div>
+    )
+}
