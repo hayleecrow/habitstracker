650c387caba0e8416269bc531844ad24332f4cdf
hayleecrow <cro21045@byui.edu>
2026-03-11 21:51:33 -0600

    Navigation and authentenication only happen after user is registered

--- src/service.js
+++ src/service.js
@@ -1,3 +1,5 @@
+import { AuthState } from './login/authState.js';
+
 export async function registerUser(userName, password) { 
     const response = await fetch('/api/auth', {
         method: 'POST',
@@ -8,5 +10,8 @@ export async function registerUser(userName, password) {
     });
     await response.json();
     console.log('registerUser response:', response);
-    return response;
+    if (response.ok) {
+        navigate('/my_habits');
+        onAuthChange(userName, AuthState.Authenticated);
+    }
 }
